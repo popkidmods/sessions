c4cd52f1d6af41897aca80116129c25da23f0024
popkidmods <supmaziwa@gmail.com>
2024-11-16 09:40:28 +0300

    ⭕POP KID⭕

--- pair.js
+++ pair.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-    default: Popkid_Tech,
+    default: Danny_Tech,
     useMultiFileAuthState,
     delay,
     makeCacheableSignalKeyStore,
@@ -20,13 +20,13 @@ function removeFile(FilePath){
 router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
-        async function POPKID_MD_PAIR_CODE() {
+        async function SAMSUNG_MD_PAIR_CODE() {
         const {
             state,
             saveCreds
         } = await useMultiFileAuthState('./temp/'+id)
      try {
-            let Pair_Code_By_POPKID_TECH = POPKID_TECH({
+            let Pair_Code_By_Popkid_Tech = Popkid_Tech({
                 auth: {
                     creds: state.creds,
                     keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
@@ -35,15 +35,15 @@ router.get('/', async (req, res) => {
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
                 browser: ["Chrome (Linux)", "", ""]
              });
-             if(!Pair_Code_By_Popkid_Tech.authState.creds.registered) {
+             if(!Pair_Code_By_Danny_Tech.authState.creds.registered) {
                 await delay(1500);
                         num = num.replace(/[^0-9]/g,'');
-                            const code = await Pair_Code_By_Popkid_Tech.requestPairingCode(num)
+                            const code = await Pair_Code_By_Danny_Tech.requestPairingCode(num)
                  if(!res.headersSent){
                  await res.send({code});
                      }
                  }
-            Pair_Code_By_Popkid_Tech.ev.on('creds.update', saveCreds)
+            Pair_Code_By_Danny_Tech.ev.on('creds.update', saveCreds)
             Pair_Code_By_Danny_Tech.ev.on("connection.update", async (s) => {
                 const {
                     connection,
@@ -54,26 +54,26 @@ router.get('/', async (req, res) => {
                 let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                 await delay(800);
                let b64data = Buffer.from(data).toString('base64');
-               let session = await Pair_Code_By_Popkid_Tech.sendMessage(Pair_Code_By_Popkid_Tech.user.id, { text: '' + b64data });
+               let session = await Pair_Code_By_Danny_Tech.sendMessage(Pair_Code_By_Danny_Tech.user.id, { text: '' + b64data });
 
-               let POPKID_MD_TEXT = `
+               let SAMSUNG_MD_TEXT = `
 ┏━━━━━━━━━━━━━━
-┃POPKID
-┃sᴜᴄᴄᴇssғᴜʟʟʏ
-┃ᴄᴏɴɴᴇᴄᴛᴇᴅ
+┃𝐏𝐎𝐏𝐊𝐈𝐃 𝐌𝐃
+┃sᴜᴄᴄᴇssғᴜʟʟʏ ᴄᴏɴɴᴇᴄᴛᴇᴅ
+┃ʙʏ ᴘᴏᴘᴋɪᴅ🧑‍🚀
 ┗━━━━━━━━━━━━━━━
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-1️⃣ || 𝐶𝑟𝑒𝑎𝑡𝑜𝑟 =  ᴘᴏᴘᴋɪᴅ🧑‍🚀
+1️⃣ || 𝐶𝑟𝑒𝑎𝑡𝑜𝑟 =  ᴘᴏᴘᴋɪᴅ
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 2️⃣ || WhatsApp Channel = https://whatsapp.com/channel/0029VadQrNI8KMqo79BiHr3l
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 3️⃣ || check my YouTube channel on how to deploy
- *https://www.youtube.com/@popkid-devs*
+ *https://www.youtube.com/@popkidmods*
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-©*𝐏𝐎𝐏𝐊𝐈𝐃🧑‍🚀*
+©*𝐏𝐎𝐏𝐊𝐈𝐃 𝐌𝐃*
 
 _Don't Forget To Give Star To My Repo_`
- await Pair_Code_By_Popkid_Tech.sendMessage(Pair_Code_By_Popkid_Tech.user.id,{text:POPKID_MD_TEXT},{quoted:session})
+ await Pair_Code_By_Popkid_Tech.sendMessage(Pair_Code_By_popkid_Tech.user.id,{text:POPKID_MD_TEXT},{quoted:session})
  
 
         await delay(100);
@@ -81,7 +81,7 @@ _Don't Forget To Give Star To My Repo_`
         return await removeFile('./temp/'+id);
             } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
-                    POPKJD_MD_PAIR_CODE();
+                    POPKID_MD_PAIR_CODE();
                 }
             });
         } catch (err) {
